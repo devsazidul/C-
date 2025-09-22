041c593e2b5d1d0eb0877b2eb41b56385c158159
shakib08 <shakibimtiazjishan1111@gmail.com>
2025-09-22 16:44:47 +0600

    adsf

--- obj/Debug/net9.0/name.AssemblyInfo.cs
+++ obj/Debug/net9.0/name.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("name")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ba07da7ed601d15db030f688a006ff6c70d110fd")]
 [assembly: System.Reflection.AssemblyProductAttribute("name")]
 [assembly: System.Reflection.AssemblyTitleAttribute("name")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
