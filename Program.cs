1bbaed7367824d49631f1efda477eb563ad50173
devsazidul <rabbiking00@gmail.com>
2025-10-05 15:40:31 +0600

    a

--- Program.cs
+++ Program.cs
@@ -28,7 +28,7 @@ namespace HelloWorld
             const int myNum = 15;
 
             Console.WriteLine(myNum);
-            // aksdjf
+            // aksdjf asdfasdf
         }
     }
 }
--- obj/Debug/net9.0/name.AssemblyInfo.cs
+++ obj/Debug/net9.0/name.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("name")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1c944d9f40a54eed8d9cbf4b1d64605e74e2107e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+153a24b8689cf39c85effe8864903c7d71e88473")]
 [assembly: System.Reflection.AssemblyProductAttribute("name")]
 [assembly: System.Reflection.AssemblyTitleAttribute("name")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
