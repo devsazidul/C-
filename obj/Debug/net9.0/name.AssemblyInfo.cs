3fd9033dbb96120836ab5a9235375cabecf8240b
devsazidul <rabbiking00@gmail.com>
2025-10-13 16:07:26 +0600

    aas

--- obj/Debug/net9.0/name.AssemblyInfo.cs
+++ obj/Debug/net9.0/name.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("name")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+03e1d8ad01016bf83e42e0abf9bb9cfaa6fe7b35")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2032830b536d3f545b2c1a50a032a37be01e1c99")]
 [assembly: System.Reflection.AssemblyProductAttribute("name")]
 [assembly: System.Reflection.AssemblyTitleAttribute("name")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
